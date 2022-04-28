c02eba8c5ace0fb4f51ad36532351d27f0dddb06
Jas Bakshi <jasbakshi@Jass-MacBook-Air.local>
2022-04-28 15:33:17 -0500

    changes

--- QuiverInsiderTradingAlgorithm.cs
+++ QuiverInsiderTradingAlgorithm.cs
@@ -52,11 +52,11 @@ namespace QuantConnect.DataLibrary.Tests
             {
                 // based on the custom data property we will buy or short the underlying equity
                 var t = data[_customDataSymbol].Shares;
-                if (t > 0.0)
+                if (t > 0.0m)
                 {
                     SetHoldings(_equitySymbol, 1);
                 }
-                else if (t < 0)
+                else if (t < 0.0m)
                 {
                     SetHoldings(_equitySymbol, -1);
                 }
